87f6ca44b7dd2b3d62d86da8a16184f90ca54880
Axel Liscovsky <102985114+Axel-lis@users.noreply.github.com>
2022-11-19 22:09:12 -0300

    Add files via upload
    Efecto cool de transición al scrollear😎​ HTML, CSS, JS.

---
+++ coolscrollanimation/index.js
@@ -0,0 +1,15 @@
+
+
+const observer= new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show'); /*si solo se quiere mostrar una vez la animacion dejamos esto, else..*/
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+})
+
+const hiddenElements= document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=>observer.observe(el));
